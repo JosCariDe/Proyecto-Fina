c5d699adb8153e1b7d34a45e77589e1f43cb37a7
joseLaptop <josenarvaezcariaga@gmail.com>
2024-06-09 21:57:47 -0500

    Se agregó la capa de Servicios de Rent

--- carrosCaribenios/src/main/java/com/example/carrosCaribenios/service/rent/RentServiceImpl.java
+++ carrosCaribenios/src/main/java/com/example/carrosCaribenios/service/rent/RentServiceImpl.java
@@ -1,40 +1,71 @@
 package com.example.carrosCaribenios.service.rent;
 
 import com.example.carrosCaribenios.dto.rent.RentDto;
+import com.example.carrosCaribenios.dto.rent.RentMapper;
 import com.example.carrosCaribenios.dto.rent.RentToSaveDto;
+import com.example.carrosCaribenios.entitys.Rent;
 import com.example.carrosCaribenios.exception.RentNotFoundException;
+import com.example.carrosCaribenios.repository.RentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class RentServiceImpl implements RentService{
+
+    private final RentRepository rentRepository;
     @Override
     public RentDto guardarCarro(RentToSaveDto rentDto) {
-        return null;
+        Rent rent = RentMapper.INSTANCE.rentToSaveDtoToRent(rentDto);
+        Rent rentSave = rentRepository.save(rent);
+        return RentMapper.INSTANCE.rentToRentDto(rentSave);
     }
 
     @Override
     public RentDto actualizarCarro(Long id, RentToSaveDto rentDto) {
-        return null;
+        return rentRepository.findById(id).map(rentInBd -> {
+            rentInBd.setCiudad(rentDto.ciudad());
+            rentInBd.setFechaInicio(rentDto.fechaInicio());
+            rentInBd.setFechaFinal(rentDto.fechaFinal());
+            rentInBd.setModelo(rentDto.modelo());
+            rentInBd.setMarca(rentDto.marca());
+            rentInBd.setPrecio(rentDto.precio());
+
+            Rent rentSave = rentRepository.save(rentInBd);
+
+            return RentMapper.INSTANCE.rentToRentDto(rentSave);
+        }).orElseThrow(() -> new RentNotFoundException("Carro no encontrado"));
     }
 
     @Override
     public RentDto buscarCarroPorId(Long id) throws RentNotFoundException {
-        return null;
+        Rent rent = rentRepository.findById(id)
+                .orElseThrow(() -> new RentNotFoundException("Carro No Encontrado"));
+        return RentMapper.INSTANCE.rentToRentDto(rent);
     }
 
     @Override
     public void removerCarro(Long id) {
-
+        Rent rent = rentRepository.findById(id)
+                .orElseThrow(() -> new RentNotFoundException("No se encontró el coche a eliminar"));
+        rentRepository.delete(rent);
     }
 
     @Override
     public List<RentDto> gerAllCarros() {
-        return null;
+        List<Rent> rents = rentRepository.findAll();
+        return RentMapper.INSTANCE.rentsToRentsDto(rents);
     }
 
     @Override
     public List<RentDto> filtrarPorCiudadYFechas(String ciudad, LocalDateTime fechaInicio, LocalDateTime fechaFinal) throws RentNotFoundException {
-        return null;
+        List<Rent> rents = rentRepository.findCarsFilters(ciudad,fechaInicio,fechaFinal);
+        if(rents.isEmpty()) {
+            throw new RentNotFoundException("No se encontró coches En renta con esas condiciones");
+        }
+        return RentMapper.INSTANCE.rentsToRentsDto(rents);
     }
 }
